447b38312cbef043896580ab868db98cfe061e8f
mytran123 <mytran091196@gmai.com>
2021-10-21 15:16:28 +0700

    bài tập

--- BT_ban-nhay-Square-Dance/index.php
+++ BT_ban-nhay-Square-Dance/index.php
@@ -1,15 +1,38 @@
 <?php
 include_once "Dancer.php";
 
-$female = new SqlQueue();
-$male = new SqlQueue();
+$girls = new SplQueue();
+$boys = new SplQueue();
 
-$female->enqueue();
-$female->enqueue();
-$female->enqueue();
-$female->enqueue();
-$male->enqueue();
-$male->enqueue();
-$male->enqueue();
-$male->enqueue();
+$girls->enqueue(new Dancer("Hoa","female"));
+$girls->enqueue(new Dancer("Phượng","female"));
+$girls->enqueue(new Dancer("Mai","female"));
+$girls->enqueue(new Dancer("Lan","female"));
 
+$boys->enqueue(new Dancer("Tài","male"));
+$boys->enqueue(new Dancer("Tuấn","male"));
+$boys->enqueue(new Dancer("Tú","male"));
+
+$partner = [];
+$female_wait = [];
+$male_wait = [];
+
+while (!$girls->isEmpty() || !$boys->isEmpty()) {
+    if (!$girls->isEmpty() && !$boys->isEmpty()) {
+        $partner[] = $girls->dequeue()->name . " & " . $boys->dequeue()->name;
+    } else if (!$girls->isEmpty() && $boys->isEmpty()) {
+        $female_wait[] = $girls->dequeue()->name;
+    } else if ($girls->isEmpty() && !$boys->isEmpty()) {
+        $male_wait[] = $boys->dequeue()->name;
+    }
+}
+
+echo "<pre>";
+echo "Cặp đôi: ";
+print_r($partner);
+echo "<br>";
+echo "Hàng đợi nữ: ";
+print_r($female_wait);
+echo "<br>";
+echo "Hàng đợi nam: ";
+print_r($male_wait);
